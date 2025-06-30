405995f5dda1d8c666ae6a36044c48f39b94082f
Ighor Rodrigues de Carvalho <ighorrcarvalho.ti@gmail.com>
2025-06-30 15:00:53 -0300

    chore  docker and database configuration docker compose, prisma with migrations.

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,12 @@
 import fastify from 'fastify';
+import { PrismaClient } from '../generated/prisma'
+
+const prisma = new PrismaClient();
+
+prisma.user.create({
+    "name": "Ighor Rodrigues",
+    "email": 'ighorrc@gmail.com'
+})
+
 
 export const app = fastify();
