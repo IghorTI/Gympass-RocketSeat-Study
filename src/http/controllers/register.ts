4587137429143c6cd2e95a27be04c1a2ed945339
Ighor Rodrigues de Carvalho <ighorrcarvalho.ti@gmail.com>
2025-07-28 19:51:47 -0300

    feat: Handler errors

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -4,6 +4,7 @@ import { prisma } from "@/lib/prisma";
 import z from "zod";
 import { RegisterUseCase  } from "@/use-cases/register";
 import { PrismaUsersRepository } from "@/repositories/prisma/prisma-users-repository";
+import { UserAlreadyExistsError } from "@/use-cases/errors/user-already-exists-error";
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
 
@@ -15,15 +16,8 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
 
     const { name, email, password } = createUserBodySchema.parse(request.body);
 
-    const password_hash = await hash(password, 6);
-
-    const userWithSameEmail = await prisma.user.findUnique({
-        where: {
-            email
-        }
-    })
-
     try {
+
         const primaUsersRepository = new PrismaUsersRepository();
         const registerUseCase = new RegisterUseCase(primaUsersRepository);
 
@@ -31,17 +25,13 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
         await registerUseCase.execute({ name, email, password })
 
     } catch (err) {
-        return reply.status(409).send()
+
+        if(err instanceof UserAlreadyExistsError){
+            return reply.status(409).send({message: err.message})
+        }
+
+        return reply.status(500).send()
     }
-
-
-    // let user = await prisma.user.create({
-    //     data: {
-    //         name,
-    //         email,
-    //         password_hash: password_hash, // In a real application, you should hash the password before storing it
-    //     }
-    // })
 
     return reply.status(201).send({
         message: 'User created successfully'
